ffa8f777a1932112a58254ec685461d95ba9b3cc
Juan José Márquez <martrejj@ar.ibm.com>
2018-08-27 17:55:26 -0300

    definiendo rutas

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* Definiendo un nueva ruta*/
+
+Route::get('/miprimeraruta',function(){
+	return 'Hola Mundo!!!';
+});
+/* Ruta con paramentros */ 
+
+Route::get('/name/{name}',function($name){
+	return 'Hola soy '.$name;
+});
+
+/* ruta con mas parametros y se le establece como reaccionar si no esta */
+
+Route::get('/name/{name}/lastname/{lastname?}',function($name,$lastname='apellido'){
+	return 'Hola Soy '.$name.' '.$lastname;
+});
+
+
